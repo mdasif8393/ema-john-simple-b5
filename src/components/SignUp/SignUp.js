4eb6e937657bbd30bd602b307c091f8164b2d874
mdasif8393 <md.asif.8393@gmail.com>
2022-09-09 00:29:34 +0600

    integrate sign in and sign up with firebase

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,23 +1,64 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './SignUp.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const [createUserWithEmailAndPassword, user] =  useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+
+    const handleEmailBlur = (event) => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = (event) => {
+        setPassword(event.target.value);
+    }
+    const handleConfirmPasswordBlur = (event) => {
+        setConfirmPassword(event.target.value);
+    }
+
+    const handleCreateUser = (event) => {
+        event.preventDefault();
+        if(password !== confirmPassword) {
+            setError("Passwords didn't match");
+            return;
+        }
+        if(password.length < 6 ) {
+            setError("Passwords must be at least 6 character");
+            return;
+        }
+        createUserWithEmailAndPassword(email, password);
+    }
+
+    if(user){
+        setError("");
+        navigate('/shop')
+    }
+
+    
     return (
         <div className="form-container">
             <div>
             <h2 className="form-title">Sign Up</h2>
-                <form action="">
+            <p style={{color:'red'}}>{error}</p>
+                <form onSubmit={handleCreateUser}>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" required/>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required/>
                     </div>
                     <div className="input-group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" required/>
+                        <label  htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" required/>
                     </div>
                     <div className="input-group">
                         <label htmlFor="confirm-password">Confirm Password</label>
-                        <input type="password" name="confirm-password" id="" />
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password"  />
                     </div>
                     <input className="form-submit" type="submit" value="Sign Up" required/>
                 </form>
